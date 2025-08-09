0dd7f76005ebb4fecb045aab0ea30829c08dbac4
Prashant Kali <you@example.com>
2025-08-09 15:17:51 +0530

    Refactor balance display and enhance transaction styling with delete icon

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ let left = document.querySelector(".left");
 
 const description = document.getElementById("description");
 const amount = document.getElementById("amount");
+
+let totalAmt = document.querySelector(".tamount");
+let incomeAmt = document.querySelector(".incomeamt");
+let expenseAmt = document.querySelector(".expenseamt");
+
+let totalAmtNum = Number(totalAmt.textContent);
+let incomeAmtNum = Number(incomeAmt.textContent);
+let expenseAmtNum = Number(expenseAmt.textContent);
+
 function addTranction(event) {
   event.preventDefault();
   const newCard = document.createElement("div");
@@ -12,11 +21,42 @@ function addTranction(event) {
   p1.classList.add("name");
 
   let p2 = document.createElement("p");
-  p2.textContent = amount.value;
+  if (amount.value < 0) {
+    p2.textContent = `-$${Math.abs(amount.value)}`;
+  } else {
+    p2.textContent = `$${amount.value}`;
+  }
+  let img = document.createElement("i");
+  img.classList.add("fa-solid", "fa-trash");
   p2.classList.add("amt");
+
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("deleteicon");
+
   newCard.appendChild(p1);
-  newCard.appendChild(p2);
+  newDiv.appendChild(p2);
+  newDiv.appendChild(img);
+  newCard.appendChild(newDiv);
   left.appendChild(newCard);
+
+  totalAmtNum = 0;
+  incomeAmtNum = 0;
+  expenseAmtNum = 0;
+
+  let transactionAmt = document.querySelectorAll(".amt");
+  transactionAmt.forEach((tAmt) => {
+    let value = Number(tAmt.textContent.replace("$", ""));
+
+    if (value < 0) {
+      expenseAmtNum += value;
+    } else {
+      incomeAmtNum += value;
+    }
+    incomeAmt.textContent = incomeAmtNum;
+    expenseAmt.textContent = expenseAmtNum;
+    totalAmt.textContent = incomeAmtNum + expenseAmtNum;
+  });
+
   description.value = "";
   amount.value = "";
 }
